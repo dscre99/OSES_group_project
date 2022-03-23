fab50fcd03a05a60e0d26cf42e7b1cb4ebeadc55
Diamante Simone Crescenzo <s295783@studenti.polito.it>
2022-03-23 14:02:24 +0100

    chore: added a receiver function skeleton

--- custom_threads/thread_functions.c
+++ custom_threads/thread_functions.c
@@ -9,6 +9,7 @@
  */
 
 #include <rtthread.h>
+#include "../custom_mailbox/custom_mailbox_init.h"
 
 void print_string(void *parameter)
 {
@@ -17,4 +18,21 @@ void print_string(void *parameter)
     rt_kprintf("\nTHIS IS A TEST STRING\n");
 
     rt_kprintf("\nThread 'print_string' terminating...\n");
+}
+
+void receive_message(void *parameter)
+{
+    // to be completed
+    while(1)
+    {
+        /* Receive messages from the message queue */
+        if (rt_mq_recv(&mq, &buf, sizeof(buf), RT_WAITING_FOREVER) == RT_EOK)
+        {
+            rt_kprintf("thread1: recv msg from msg queue, the content:%c\n", buf);
+            if (cnt == 19)
+            {
+                break;
+            }
+        }
+    }
 }
