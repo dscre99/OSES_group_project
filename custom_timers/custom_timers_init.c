545e03fd9dc799612fa845193b924ec718dd1b18
Diamante Simone Crescenzo <s295783@studenti.polito.it>
2022-03-23 13:39:00 +0100

    func: timer now calls a simple test function
    Function called by timer simply prints and updates a value

--- custom_timers/custom_timers_init.c
+++ custom_timers/custom_timers_init.c
@@ -15,18 +15,34 @@
 
 #define ONE_SECOND 1000
 
+void generate_value(void * parameters);
+
 static struct rt_timer print_timer;
+int value = 0;
 
 // test timer with thread activation
 int custom_timers_init(void)
 {
+
     // print_string timer initialization
-    rt_timer_init(&print_timer, "print_timer", launch_print_string_thread, RT_NULL, ONE_SECOND, RT_TIMER_FLAG_PERIODIC);
+    //rt_timer_init(&print_timer, "print_timer", launch_print_string_thread, RT_NULL, ONE_SECOND, RT_TIMER_FLAG_PERIODIC);
+    rt_kprintf("Test pointer:: %d\n", &value);
+    rt_timer_init(&print_timer, "print_timer", generate_value, &value, ONE_SECOND, RT_TIMER_FLAG_PERIODIC);
+
 
     // print_string timer startup
     rt_timer_start(&print_timer);
 
     return 0;
+}
+
+void generate_value(void * parameters){
+    int *val_p = (int *)parameters;
+
+    rt_kprintf("Generated value:: %d\n", *val_p);
+    *val_p += 1;
+
+    return;
 }
 
 // test timer with function
