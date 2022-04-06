d900dad55ecac2c44fdb81f912156d72cd309219
Dave <davidefogliato@gmail.com>
2022-04-06 12:00:57 +0200

    chore: brake detection button test

--- applications/main.c
+++ applications/main.c
@@ -12,21 +12,30 @@
 #include <rtdevice.h>
 #include <board.h>
 
-/* defined the LED0 pin: PB1 */
+/* defined the PIN */
 #define LED0_PIN    GET_PIN(A, 5)
+#define BUT0_PIN    GET_PIN(C, 13)
 
 int main(void)
 {
     int count = 1;
     /* set LED0 pin mode to output */
     rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
+    rt_pin_mode(BUT0_PIN, PIN_MODE_INPUT);
+    rt_pin_write(LED0_PIN, PIN_LOW);
 
     while (count++)
     {
-        rt_pin_write(LED0_PIN, PIN_HIGH);
-        rt_thread_mdelay(500);
-        rt_pin_write(LED0_PIN, PIN_LOW);
-        rt_thread_mdelay(500);
+       // rt_pin_write(LED0_PIN, PIN_HIGH);
+        //rt_thread_mdelay(500);
+        rt_thread_mdelay(50);
+
+        if( rt_pin_read(BUT0_PIN)!=1){
+                    rt_pin_write(LED0_PIN, PIN_HIGH);
+        } else {
+            rt_pin_write(LED0_PIN, PIN_LOW);
+        }
+
     }
 
     return RT_EOK;
--- custom_timers/custom_timers_init.c
+++ custom_timers/custom_timers_init.c
@@ -10,15 +10,22 @@
 
 #include <rtthread.h>
 #include <rthw.h>
+#include <board.h>
 #include "../custom_threads/thread_functions.h"
 #include "../custom_threads/custom_threads_init.h"
 #include "../custom_mailbox/custom_mailbox_init.h"
 
 #define ONE_SECOND 1000
+#define _25_MSECOND 25
+
+#define LED0_PIN    GET_PIN(A, 5)
+#define BUT0_PIN    GET_PIN(C, 13)
 
 void generate_value(void * parameters);
+void brake_detection(void * parameters);
 
 static struct rt_timer print_timer;
+static struct rt_timer brake_timer;
 int value = 0;
 
 // test timer with thread activation
@@ -27,12 +34,13 @@ int custom_timers_init(void)
 
     // print_string timer initialization
     //rt_timer_init(&print_timer, "print_timer", launch_print_string_thread, RT_NULL, ONE_SECOND, RT_TIMER_FLAG_PERIODIC);
-    rt_kprintf("Test pointer:: %d\n", &value);
-    rt_timer_init(&print_timer, "print_timer", generate_value, &value, ONE_SECOND, RT_TIMER_FLAG_PERIODIC);
+    //rt_kprintf("Test pointer:: %d\n", &value);
+    rt_timer_init(&brake_timer, "brake_timer", brake_detection, &value, _25_MSECOND, RT_TIMER_FLAG_PERIODIC);
 
 
     // print_string timer startup
-    rt_timer_start(&print_timer);
+    //rt_timer_start(&print_timer);
+    rt_timer_start(&brake_timer);
 
     return 0;
 }
@@ -44,6 +52,20 @@ void generate_value(void * parameters){
     rt_mb_send(&mb, (rt_uint32_t)*val_p);
 
     *val_p += 1;
+
+    return;
+}
+
+void brake_detection(void * parameters){
+    int *val_p = (int *)parameters;
+    if( rt_pin_read(BUT0_PIN)!=1){
+        rt_pin_write(LED0_PIN, PIN_HIGH);
+        *val_p += 1;
+    } else {
+        rt_pin_write(LED0_PIN, PIN_LOW);
+        *val_p =0;
+    }
+    rt_mb_send(&mb, (rt_uint32_t)*val_p);
 
     return;
 }
